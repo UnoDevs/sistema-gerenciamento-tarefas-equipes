25f779df263ba6e29759f70141603d710831c5d0
Felipe Santos <felipe.santosmf2022@gmail.com>
2024-11-29 21:58:17 -0300

    Create first screen

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -5,13 +5,14 @@ import ejs from 'ejs';
 import session from 'express-session';
 
 const app = express();
+const axios = require('axios');
 
-// Servir arquivos estáticos da pasta public
+
 app.use(express.static('frontend'));
 
-// Configura EJS como a engine de renderização de templates
+
 app.set('view engine', 'ejs');
-//app.set('views', path.join(`${__dirname}, src, views`));
+
 app.set('views', path.join(__dirname, '/views'));
 
 declare module 'express-session' {
@@ -20,26 +21,48 @@ declare module 'express-session' {
     }
 }
 
-// Middleware para permitir dados no formato JSON
+
 app.use(express.json());
-// Middleware para permitir dados no formato URLENCODED
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Middleware de sessão (deve ser antes das rotas que acessam a sessão)
+
 app.use(session({
-    secret: 'segredo', // Substitua por uma chave secreta
+    secret: 'segredo', 
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: false } // Em produção, use 'true' com HTTPS
+    cookie: { secure: false } 
 }));
 
 app.get('/singin', (req: Request, res: Response) => {
-    
+    res.render('singin');
 })
 
+app.post('/singin', (req, res) => {
+   const { name, email, senha } = req.body;
 
+    try {
+    const response = axios.post('http://localhost:3001/users', 
+    {
+        name: name,
+        email: email,
+        password: senha,
+        role: 'ADMIN'
+    })
+    }catch (error) {
+        res.json(error)
+    }
+    res.redirect('/login')
+  });
+
+app.get('/login', (req: Request, res: Response) => {
+    res.render('login');
+})
+
+app.post('/login', (req: Request, res: Response) => {
+    
+})
 
 
 app.listen('3000', () => console.log("Server is listening on port 3000"));
